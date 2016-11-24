a0d49113ce832b3389dea4ad7b23f5bb9467751e
yukiasai <yukiasai@gmail.com>
2016-11-24 09:27:36 +0900

    Implement ShadowStyleTests

---
+++ NoriTests/ShadowStyleTests.swift
@@ -0,0 +1,56 @@
+//
+//  ShadowStyleTests.swift
+//  Nori
+//
+//  Created by yukiasai on 2016/11/24.
+//  Copyright © 2016年 yukiasai. All rights reserved.
+//
+
+import XCTest
+@testable import Nori
+
+class ShadowStyleTests: XCTestCase {
+    func testShadowOffset() {
+        let view = UIView()
+        let style = ViewStyle {
+            $0.shadow = ShadowStyle {
+                $0.offset = CGSize(width: 10, height: 5)
+            }
+        }
+        view.stylize(with: style)
+        XCTAssertEqual(view.layer.shadowOffset, style.shadow?.offset)
+    }
+    
+    func testShadowColor() {
+        let view = UIView()
+        let style = ViewStyle {
+            $0.shadow = ShadowStyle {
+                $0.color = UIColor.green
+            }
+        }
+        view.stylize(with: style)
+        XCTAssertEqual(view.layer.shadowColor, style.shadow?.color?.cgColor)
+    }
+     
+    func testRadius() {
+        let view = UIView()
+        let style = ViewStyle {
+            $0.shadow = ShadowStyle {
+                $0.radius = 8
+            }
+        }
+        view.stylize(with: style)
+        XCTAssertEqualWithAccuracy(view.layer.shadowRadius, style.shadow!.radius!, accuracy: CGFloat(FLT_EPSILON))
+    }
+    
+    func testShadowOpacity() {
+        let view = UIView()
+        let style = ViewStyle {
+            $0.shadow = ShadowStyle {
+                $0.opacity = 0.78
+            }
+        }
+        view.stylize(with: style)
+        XCTAssertEqualWithAccuracy(view.layer.shadowOpacity, style.shadow!.opacity!, accuracy: FLT_EPSILON)
+    }
+}
